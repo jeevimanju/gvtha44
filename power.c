fa35967cab658162f8d58fd24cc239c9ba98abf7
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-01-10 16:58:39 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,n;
+printf("enter the number",a);
+scanf("%d",&a);
+printf("the number is %d",a);
+printf("enter the power",n);
+scanf("%d",&n);
+printf("the number is %d",n);
+b=a^n;
+printf("the power of num is %d",b);
+return 0;
+}
