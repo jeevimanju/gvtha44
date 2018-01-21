1ad5ce0ed321cf011cda71a70c13e48ed215cd5f
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-01-21 11:02:45 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int min,hour,total_minutes;
+printf("enter the hours");
+scanf("%d",&hour);
+printf("enter the minutes");
+scanf("%d",&min);
+total_minutes=(hours*60)+minutes;
+printf("hours and minutes are");
+}
