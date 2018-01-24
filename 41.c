05133ecd093993cd8fcf156d9dd736aed168ea09
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-01-24 16:22:36 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a,i;
+char ch[20];
+printf("how many time print");
+scanf("%d",&a);
+printf("enter char");
+scanf("%c",&ch);
+for(i=1;i<=a;i++)
+{
+printf("%c\n");;
+}
+return 0;
+}
+
