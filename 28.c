59a27f9123ea965e2f810535d34415e48a259adb
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-01-28 15:39:36 +0530

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int i,a[10],n;
+int i,a[10],b;
 printf("enter no of elements in an array\n");
-scanf("%d",&n);
+scanf("%d",&b);
 printf("enter the elements in an array:\n");
-for(i=0;i<n;i++)
+for(i=0;i<b;i++)
 {
 scanf("%d\n",&a[i]);
 }
-for(i=0;i<n;i++)
+for(i=0;i<b;i++)
 {
 printf("a[%d]=%d\n",i,a[i]);
 }
