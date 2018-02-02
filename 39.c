56f8217377bdccaba37808b9782b24f1a6c8bca0
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-02-02 16:43:00 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,j,temp;
+printf("\n enter the Nelements:");
+for(i=0;i<=9;i++)
+{
+scanf("%d",&a[i]);
+for(i=0;i<=9;i++)
+{
+for(i=0;i <=9;i++)
+{
+if(a[i]<a[j])
+{
+a[i]=a[j];
+a[j]=temp;
+printf("enter the maximum number :");
+a[0]=max;
+printf("%d",max);
+}
