aae820309db21a17f01ba97da7d4fa16d4855d69
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-02-25 16:30:32 +0530

    Update 40.c

--- 40.c
+++ 40.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void main()
 {
-int sum=0,num1,num2,j,n;
+int s=0,num1,num2,j,n;
 scanf("%d%d%d",&num1,&num2,&n);
 for(j=0;j<n;j++)
 {
-sum=num1+num2;
-printf("%d\n",sum);
+s=num1+num2;
+printf("%d\n",s);
 num1=num2;
-num2=sum;
+num2=s;
 }
 }
