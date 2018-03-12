03dc76e870dee6d1518663cccf05eab736d84484
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-03-12 16:15:41 +0530

    Update 71.c

--- 71.c
+++ 71.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-int main(void)
+void main()
 {
 char a[100];
 int i,j,flag=0;
@@ -24,5 +24,4 @@ else
 {
     printf("The given string is palindrome");
 }
-return 0;
 }
