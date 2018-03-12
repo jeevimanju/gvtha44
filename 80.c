77c98a6591463dd14cfa58a458d71dd6a7ad9ccb
jeevimanju <32487810+jeevimanju@users.noreply.github.com>
2018-03-12 16:49:22 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a[10];
+    printf("enter num");
+    scanf("%d",&a);
+    for(i=0;i<n;i++)
+    {
+    if(a[i]%2!=0)
+    {
+        printf("%d",a);
+    }
+    }
+}
